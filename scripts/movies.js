1e3291dae147e535bf02160bdd41398f56e9f06c
Grace Boaz <anna.g.boaz@gmail.com>
2022-10-25 12:00:22 -0500

    first commit. matches movie to review

---
+++ scripts/movies.js
@@ -0,0 +1,14 @@
+import { movies, reviews } from './database.js'
+
+const movies = movies();
+const reviews = reviews();
+
+//if the movie and review match, then..
+for (movie of movies){
+    for (review of reviews){
+        if (movie.id === review.movieId){
+            //pass the review into that movie's html container
+            let html = ``
+        }
+    }
+}
