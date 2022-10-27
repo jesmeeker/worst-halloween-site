776c32eff9407e7a0c9841e38667a981792480f8
Grace Boaz <anna.g.boaz@gmail.com>
2022-10-27 13:04:10 -0500

    added functions

--- scripts/movies.js
+++ scripts/movies.js
@@ -1,22 +1,52 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { movies, reviews } from './database.js'
 
-const movies = movies();
-const reviews = reviews();
+const Movies = movies()
+const Reviews = reviews()
 
-//if the movie and review match, then..
-for (movie of movies){
-    for (review of reviews){
-        if (movie.id === review.movieId){
-            //pass the review into that movie's html container
-            let html = ``
-        }
-    }
+const movieContainer = document.querySelector(".moviesHere")
+const reviewContainer = document.querySelector(".reviewsHere")
+
+//iterate through each movie, conver the image to html
+const prepareMovies = () =>{
+    let htmlMovies = ''
+    for (const movie of Movies){
+        htmlMovies += `
+         <article>
+             <div class="testImage">
+                 <img scr="${movie.img}" alt="Boo!" width="500" height="600"/>
+             </div>
+             <div class="movieInfo">
+                  ${movie.title}: ${movie.synopsis}
+             </div>
+         </article>`
+     }
+    return htmlMovies
 }
-=======
 
->>>>>>> main
-=======
-////MOVIES
->>>>>>> main
+const prepareReviews = () => {
+    let htmlReviews = ''
+    for (const movie of Movies){
+        for (const review of Reviews){
+            if (movie.id === review.movieId){
+                htmlReviews += `
+                <article>
+                    <div class ="testReview">
+                        <div class="user">
+                            ${review.user}
+                        </div>
+                        <div class"movieTitle">
+                            ${movie.title}
+                        </div>
+                        <div class "textAndRating">
+                            ${review.text} Rating: ${review.rating}
+                        </div>
+                    </div>
+                </article>`
+            }
+           }
+    }
+    return htmlReviews
+}
+
+movieContainer.innerHTML = prepareMovies
+reviewContainer.innerHTML = prepareReviews
